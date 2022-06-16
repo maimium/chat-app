cb9e2242688e9a08bd701dd58bf7e1cd17b67952
maimium <mamopoyoo@yahoo.co.jp>
2022-06-16 16:40:26 +0900

    画面推移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,8 @@
 class RoomsController < ApplicationController
 
+  def index
+  end
+
   def new
     @room = Room.new
   end
@@ -16,6 +19,6 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).parmit(:name, user_ids: [])
-
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,6 +1,6 @@
 class RoomUser < ApplicationRecord
 
-  belong_to :room
-  belong_to :user
+  belongs_to :room
+  belongs_to :user
 
 end
